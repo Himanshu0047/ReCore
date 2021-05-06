7a86ead0a9ee63fae7b3096d54b4005c7d6d810a
Himanshu0047 <sharmahimanshu.7699@gmail.com>
2021-05-06 17:31:47 +0530

    Uploaded game

---
+++ ReCore/Assets/Final/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform playerAttach;
+    public Transform target;
+    public Transform floor;
+    //public float smoothing = 5f;
+
+    Vector3 offset;
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+        offset = transform.position - target.position;
+    }
+
+    private void FixedUpdate()
+    {
+       
+        playerAttach.position = target.position;
+        Vector3 previousEulerAngle = playerAttach.eulerAngles;
+        Vector3 gyroInput = -Input.gyro.rotationRateUnbiased;
+        Vector3 targetEulerAngle = previousEulerAngle + gyroInput * Time.deltaTime * Mathf.Rad2Deg;
+        targetEulerAngle.x = 0;
+        targetEulerAngle.z = 0;
+        playerAttach.eulerAngles = targetEulerAngle;
+        floor.eulerAngles = targetEulerAngle;
+
+        Vector3 targetCamPos = target.position + offset;
+        //transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
---
+++ ReCore/Assets/Final/Scripts/JoystickControler.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class JoystickControler : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    public Image circle;
+    public Image outerCircle;
+
+    public Transform floor;
+
+    public float speed = 6f;
+    float camRayLength = 100;
+
+    Vector3 movement;
+    Vector3 inputVector;
+
+    Animator anim;
+
+    Rigidbody playerRigidbody;
+
+    public Transform player;
+
+    int floorMask;
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(outerCircle.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / outerCircle.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / outerCircle.rectTransform.sizeDelta.y);
+
+            inputVector = new Vector3(pos.x * 2 + 1, 0, pos.y * 2 - 1);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+            // Move Joystick
+            circle.rectTransform.anchoredPosition = new Vector3(inputVector.x * (outerCircle.rectTransform.sizeDelta.x / 3),
+                                                                inputVector.z * (outerCircle.rectTransform.sizeDelta.y / 3));
+            
+        }
+    }
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        inputVector = Vector3.zero;
+        circle.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = player.GetComponent<Animator>();
+        playerRigidbody = player.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        floor.transform.position = player.position;
+        float h = inputVector.x;
+        float v = inputVector.z;
+
+        Move(h, v);
+        Turning();
+        //Animating(h, v);
+        
+    }
+
+    public void Move(float h, float v)
+    {
+        player.transform.Translate(h*speed*Time.deltaTime, 0, v*speed*Time.deltaTime);
+    }
+
+    public void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - player.position;
+            playerToMouse.y = 0f;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    /*void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }*/
+
+}
